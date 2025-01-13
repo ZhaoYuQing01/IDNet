9c5b809782e4db64357c2fc69f033e0cfc1bba7a
ZhaoYuQing01 <zyq163yxiang@163.com>
2025-01-13 16:59:56 +0800

    Update ExpertNet.py

--- model/ExpertNet.py
+++ model/ExpertNet.py
@@ -1,1 +1,99 @@
+import torch
+from torch import nn
+from einops.layers.torch import Rearrange
 
+
+class Conv2d_cd(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1,
+                 padding=1, dilation=1, groups=1, bias=False, theta=1.0):
+
+        super(Conv2d_cd, self).__init__() 
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding, dilation=dilation, groups=groups, bias=bias)
+        self.theta = theta
+    
+    def get_weight(self):
+        conv_weight = self.conv.weight
+        conv_shape = conv_weight.shape
+        conv_weight = Rearrange('c_in c_out k1 k2 -> c_in c_out (k1 k2)')(conv_weight)
+        conv_weight_cd = torch.cuda.FloatTensor(conv_shape[0], conv_shape[1], 3 * 3).fill_(0)
+        conv_weight_cd[:, :, :] = conv_weight[:, :, :]
+        conv_weight_cd[:, :, 4] = conv_weight[:, :, 4] - conv_weight[:, :, :].sum(2)
+        conv_weight_cd = Rearrange('c_in c_out (k1 k2) -> c_in c_out k1 k2', k1=conv_shape[2], k2=conv_shape[3])(conv_weight_cd)
+        return conv_weight_cd, self.conv.bias
+
+
+class Conv2d_ad(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1,
+                 padding=1, dilation=1, groups=1, bias=False, theta=1.0):
+
+        super(Conv2d_ad, self).__init__() 
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding, dilation=dilation, groups=groups, bias=bias)
+        self.theta = theta
+    
+    def get_weight(self):
+        conv_weight = self.conv.weight
+        conv_shape = conv_weight.shape
+        conv_weight = Rearrange('c_in c_out k1 k2 -> c_in c_out (k1 k2)')(conv_weight)
+        conv_weight_ad = conv_weight - self.theta * conv_weight[:, :, [3, 0, 1, 6, 4, 2, 7, 8, 5]]
+        conv_weight_ad = Rearrange('c_in c_out (k1 k2) -> c_in c_out k1 k2', k1=conv_shape[2], k2=conv_shape[3])(conv_weight_ad)
+        return conv_weight_ad, self.conv.bias
+
+
+class Conv2d_hd(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1,
+                 padding=1, dilation=1, groups=1, bias=False, theta=1.0):
+
+        super(Conv2d_hd, self).__init__() 
+        self.conv = nn.Conv1d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding, dilation=dilation, groups=groups, bias=bias)
+
+    def get_weight(self):
+        conv_weight = self.conv.weight
+        conv_shape = conv_weight.shape
+        conv_weight_hd = torch.cuda.FloatTensor(conv_shape[0], conv_shape[1], 3 * 3).fill_(0)
+        conv_weight_hd[:, :, [0, 3, 6]] = conv_weight[:, :, :]
+        conv_weight_hd[:, :, [2, 5, 8]] = -conv_weight[:, :, :]
+        conv_weight_hd = Rearrange('c_in c_out (k1 k2) -> c_in c_out k1 k2', k1=conv_shape[2], k2=conv_shape[2])(conv_weight_hd)
+        return conv_weight_hd, self.conv.bias
+
+
+class Conv2d_vd(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1,
+                 padding=1, dilation=1, groups=1, bias=False):
+
+        super(Conv2d_vd, self).__init__() 
+        self.conv = nn.Conv1d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding, dilation=dilation, groups=groups, bias=bias)
+    
+    def get_weight(self):
+        conv_weight = self.conv.weight
+        conv_shape = conv_weight.shape
+        conv_weight_vd = torch.cuda.FloatTensor(conv_shape[0], conv_shape[1], 3 * 3).fill_(0)
+        conv_weight_vd[:, :, [0, 1, 2]] = conv_weight[:, :, :]
+        conv_weight_vd[:, :, [6, 7, 8]] = -conv_weight[:, :, :]
+        conv_weight_vd = Rearrange('c_in c_out (k1 k2) -> c_in c_out k1 k2', k1=conv_shape[2], k2=conv_shape[2])(conv_weight_vd)
+        return conv_weight_vd, self.conv.bias
+
+
+class Expertnet(nn.Module):
+    def __init__(self, dim):
+        super(Expertnet, self).__init__() 
+        self.conv1_1 = Conv2d_cd(dim, dim, 3, bias=True)
+        self.conv1_2 = Conv2d_hd(dim, dim, 3, bias=True)
+        self.conv1_3 = Conv2d_vd(dim, dim, 3, bias=True)
+        self.conv1_4 = Conv2d_ad(dim, dim, 3, bias=True)
+        self.conv1_5 = nn.Conv2d(dim, dim, 3, padding=1, bias=True)
+        self.relu1 = nn.LeakyReLU(0.3)
+        self.bn1 = nn.BatchNorm2d(dim)
+
+    def forward(self, x):
+        w1, b1 = self.conv1_1.get_weight()
+        w2, b2 = self.conv1_2.get_weight()
+        w3, b3 = self.conv1_3.get_weight()
+        w4, b4 = self.conv1_4.get_weight()
+        w5, b5 = self.conv1_5.weight, self.conv1_5.bias
+
+        w = w1 + w2 + w3 + w4 + w5
+        b = b1 + b2 + b3 + b4 + b5
+        res = nn.functional.conv2d(input=x, weight=w, bias=b, stride=1, padding=1, groups=1)
+        res = self.bn1(res)
+        res = self.relu1(res)
+        return res
