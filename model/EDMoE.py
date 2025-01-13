240c39fbc6135c3771663c245c082634367350fe
ZhaoYuQing01 <zyq163yxiang@163.com>
2025-01-13 16:58:14 +0800

    Add files via upload

--- model/EDMoE.py
+++ model/EDMoE.py
@@ -1,1 +1,205 @@
+import torch.nn as nn
+import torch
+import torch.nn.functional as F
+import numpy as np
+from torch.nn import init
+from .ExpertNet import Expertnet
+import scipy.linalg
+class GateNetwork(nn.Module):
+    def __init__(self, input_size, num_experts, top_k):
+        super(GateNetwork, self).__init__()
+        self.gap = nn.AdaptiveMaxPool2d(1)
+        self.gap2 = nn.AdaptiveAvgPool2d(1)
+        self.input_size = input_size
+        self.num_experts = num_experts
+        self.top_k = top_k
+        self.fc0 = nn.Linear(input_size,num_experts)
+        self.fc1 = nn.Linear(input_size, num_experts)
+        self.relu1 = nn.LeakyReLU(0.2)
+        self.softmax = nn.Softmax(dim=1)
+        init.zeros_(self.fc1.weight)
+        self.sp = nn.Softplus()
+    def forward(self, x):
+        x = self.gap(x)+self.gap2(x)
+        x = x.view(-1, self.input_size)
+        inp = x
+        x = self.fc1(x)
+        x= self.relu1(x)
+        noise = self.sp(self.fc0(inp))
+        noise_mean = torch.mean(noise,dim=1)
+        noise_mean = noise_mean.view(-1,1)
+        std = torch.std(noise,dim=1)
+        std = std.view(-1,1)
+        noram_noise = (noise-noise_mean)/std
+        topk_values, topk_indices = torch.topk(x+noram_noise, k=self.top_k, dim=1)
 
+        mask = torch.zeros_like(x).scatter_(dim=1, index=topk_indices, value=1.0)
+        x[~mask.bool()] = float('-inf')
+
+        gating_coeffs = self.softmax(x)
+
+        return gating_coeffs
+
+class HinResBlock(nn.Module):
+    def __init__(self, in_size, out_size, relu_slope=0.2, use_HIN=True):
+        super(HinResBlock, self).__init__()
+        self.identity = nn.Conv2d(in_size, out_size, 1, 1, 0)
+
+        self.conv_1 = nn.Conv2d(in_size, out_size, kernel_size=3, padding=1, bias=True)
+        self.relu_1 = nn.LeakyReLU(relu_slope, inplace=False)
+        self.conv_2 = nn.Conv2d(out_size, out_size, kernel_size=3, padding=1, bias=True)
+        self.relu_2 = nn.LeakyReLU(relu_slope, inplace=False)
+        self.conv_3 = nn.Conv2d(in_size+in_size,out_size,3,1,1)
+        if use_HIN:
+            self.norm = nn.InstanceNorm2d(out_size // 2, affine=True)
+        self.use_HIN = use_HIN
+
+    def forward(self, x):
+        resi = self.relu_1(self.conv_1(x))
+        out_1, out_2 = torch.chunk(resi, 2, dim=1)
+        resi = torch.cat([self.norm(out_1), out_2], dim=1)
+        resi = self.relu_2(self.conv_2(resi))
+        return x+resi
+    
+def sum1(tensor, dim=None, keepdim=False):
+    if dim is None:
+        # sum up all dim
+        return torch.sum(tensor)
+    else:
+        if isinstance(dim, int):
+            dim = [dim]
+        dim = sorted(dim)
+        for d in dim:
+            tensor = tensor.sum(dim=d, keepdim=True)
+        if not keepdim:
+            for i, d in enumerate(dim):
+                tensor.squeeze_(d-i)
+        return tensor
+def pixels1(tensor):
+        return int(tensor.size(2) * tensor.size(3))
+    
+class InvertibleConv1x1(nn.Module):
+    def __init__(self, num_channels, LU_decomposed=False):
+        super().__init__()
+        w_shape = [num_channels, num_channels]
+        w_init = np.linalg.qr(np.random.randn(*w_shape))[0].astype(np.float32)
+        if not LU_decomposed:
+            # Sample a random orthogonal matrix:
+            self.register_parameter("weight", nn.Parameter(torch.Tensor(w_init)))
+        else:
+            np_p, np_l, np_u = scipy.linalg.lu(w_init)
+            np_s = np.diag(np_u)
+            np_sign_s = np.sign(np_s)
+            np_log_s = np.log(np.abs(np_s))
+            np_u = np.triu(np_u, k=1)
+            l_mask = np.tril(np.ones(w_shape, dtype=np.float32), -1)
+            eye = np.eye(*w_shape, dtype=np.float32)
+
+            self.register_buffer('p', torch.Tensor(np_p.astype(np.float32)))
+            self.register_buffer('sign_s', torch.Tensor(np_sign_s.astype(np.float32)))
+            self.l = nn.Parameter(torch.Tensor(np_l.astype(np.float32)))
+            self.log_s = nn.Parameter(torch.Tensor(np_log_s.astype(np.float32)))
+            self.u = nn.Parameter(torch.Tensor(np_u.astype(np.float32)))
+            self.l_mask = torch.Tensor(l_mask)
+            self.eye = torch.Tensor(eye)
+        self.w_shape = w_shape
+        self.LU = LU_decomposed
+
+    def get_weight(self, input, reverse):
+        w_shape = self.w_shape
+        #print("147",w_shape[1])
+        if not self.LU:
+            pixels = pixels1(input)
+            dlogdet = torch.slogdet(self.weight)[1] * pixels
+            if not reverse:
+                weight = self.weight.view(w_shape[0], w_shape[1], 1, 1)
+            else:
+                weight = torch.inverse(self.weight.double()).float()\
+                              .view(w_shape[0], w_shape[1], 1, 1)
+            #print("155",weight.shape)
+            return weight, dlogdet
+        else:
+            self.p = self.p.to(input.device)
+            self.sign_s = self.sign_s.to(input.device)
+            self.l_mask = self.l_mask.to(input.device)
+            self.eye = self.eye.to(input.device)
+            l = self.l * self.l_mask + self.eye
+            u = self.u * self.l_mask.transpose(0, 1).contiguous() + torch.diag(self.sign_s * torch.exp(self.log_s))
+            dlogdet = sum1(self.log_s) * pixels1(input)
+            if not reverse:
+                w = torch.matmul(self.p, torch.matmul(l, u))
+            else:
+                l = torch.inverse(l.double()).float()
+                u = torch.inverse(u.double()).float()
+                w = torch.matmul(u, torch.matmul(l, self.p.inverse()))
+            #print("171",w_shape[1])
+            return w.view(w_shape[0], w_shape[1], 1, 1), dlogdet
+
+    def forward(self, input, logdet=None, reverse=False):
+        weight, dlogdet = self.get_weight(input, reverse)
+        if not reverse:
+            #print("178",input.shape,weight.shape)
+            z = F.conv2d(input, weight)
+            if logdet is not None:
+                logdet = logdet + dlogdet
+            return z, logdet
+        else:
+            z = F.conv2d(input, weight)
+            if logdet is not None:
+                logdet = logdet - dlogdet
+            return z, logdet
+
+
+class InvBlock(nn.Module):
+    def __init__(self, subnet_constructor, channel_num, channel_split_num, d = 1, clamp=0.8):
+        super(InvBlock, self).__init__()
+        self.split_len1 = channel_split_num  # 1
+        self.split_len2 = channel_num - channel_split_num  # 2
+
+        self.clamp = clamp
+
+        self.F = subnet_constructor(self.split_len2, self.split_len1, d)
+        self.G = subnet_constructor(self.split_len1, self.split_len2, d)
+        self.H = subnet_constructor(self.split_len1, self.split_len2, d)
+
+        in_channels = channel_num
+        self.invconv = InvertibleConv1x1(in_channels, LU_decomposed=True)
+        self.flow_permutation = lambda z, logdet, rev: self.invconv(z, logdet, rev)
+
+    def forward(self, x, rev=False):
+        x, logdet = self.flow_permutation(x, logdet=0, rev=False)
+
+        x1, x2 = (x.narrow(1, 0, self.split_len1), x.narrow(1, self.split_len1, self.split_len2))
+
+        y1 = x1 + self.F(x2)  
+        self.s = self.clamp * (torch.sigmoid(self.H(y1)) * 2 - 1)
+        y2 = x2.mul(torch.exp(self.s)) + self.G(y1) 
+        out = torch.cat((y1, y2), 1)
+
+        return out
+    
+class EDMoE(nn.Module):
+    def __init__(self,channels,num_experts,k):
+        super(EDMoE, self).__init__()
+        self.num_experts = num_experts
+        self.gate = GateNetwork(channels, num_experts, k)
+        self.expertnet = nn.ModuleList(
+            [Expertnet(channels) for i in range(num_experts)])
+        self.pre_fuse = nn.Sequential(InvBlock(HinResBlock, 4*channels, 2*channels),
+                                         nn.Conv2d(4*channels,channels,1,1,0))
+
+    def forward(self,x):
+        #print(x.shape)
+        x = self.pre_fuse(x)
+        cof = self.gate(x)
+        out = torch.zeros_like(x).to(x.device)
+        for idx in range(self.num_experts):
+            if cof[:,idx].all()==0:
+                continue
+            mask = torch.where(cof[:,idx]>0)[0]
+            expert_layer = self.expertnet[idx]
+            expert_out = expert_layer(x[mask])
+            cof_k = cof[mask,idx].view(-1,1,1,1)
+            out[mask]+=expert_out*cof_k
+
+        return out
